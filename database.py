f73cc83d3b8d7facf2731cc9dbf98dbafcb4ff9a
Nechir Salimi <nechir123@gmail.com>
2019-02-16 17:20:18 +0200

    Update database.py

--- database.py
+++ database.py
@@ -36,7 +36,10 @@ class Event(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, nullable=False)
+    time = db.Column(db.DateTime, nullable=True)
+    picture = db.Column(db.String(256), nullable=True)
+    name = db.Column(db.String(32), nullable=False, unique=True)
+    location = db.Column(db.String(32), nullable=False, unique=True)
     event_id = db.Column(db.Integer, db.ForeignKey("event.id"))
     event = db.relationship("Event", back_populates="creator")
 
@@ -47,5 +50,39 @@ users = db.Table("joinedusers",
                  db.Column("event_id", db.Integer, db.ForeignKey(
                            "event.id"), primary_key=True)
                  )
+
+# @app.route("/user/add/", methods=["POST"])
+# def add_user():
+#     # This branch happens when client submits the JSON document
+
+#     if not request.content_type == 'application/json':
+#         return 'Request content type must be JSON', 415
+
+#     try:
+
+#         name = request.json["name"]
+#         location = request.json["location"]
+#         exist = User.query.filter_by(name=name).first()
+
+#         if (exist == None):
+#             pro = User(
+#                 name=name,
+#                 location=location
+
+#             )
+#             db.session.add(pro)
+#             db.session.commit()
+#             return "successful", 201
+#         elif (exist != None):
+#             return "Handle already exists", 409
+#         else:
+#             abort(404)
+#     except (KeyError, ValueError, IntegrityError):
+
+#         abort(400)
+
+#     except (TypeError):
+#         return 'Request content type must be JSON', 415
+
 
 db.create_all()
