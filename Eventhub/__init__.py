a4918a6af8d1163718df34a4c840dff093d1ca32
Haoban <37427359+Haoban@users.noreply.github.com>
2019-04-07 16:12:12 +0300

    models

--- Eventhub/__init__.py
+++ Eventhub/__init__.py
@@ -1,7 +1,33 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+
+# Based on http://flask.pocoo.org/docs/1.0/tutorial/factory/#the-application-factory
+# Modified to use Flask SQLAlchemy
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+    #    SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(app.instance_path, "development.db"),
+    #    SQLALCHEMY_TRACK_MODIFICATIONS=False
+        DATABASE=os.path.join(app.instance_path, 'Eventhub.sqlite'),
+    )
+    
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+        
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    db.init_app(app)
+    return app
