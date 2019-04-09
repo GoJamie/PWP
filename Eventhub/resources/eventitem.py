6d725c8d6aba9548edfdf62fc7e52172b986c8a8
Haoban <37427359+Haoban@users.noreply.github.com>
2019-04-09 16:23:50 +0300

    Eventitem doesn't work

--- Eventhub/resources/eventitem.py
+++ Eventhub/resources/eventitem.py
@@ -1,30 +1,53 @@
-from flask_restful import Resource
+from flask_restful import Resource, Api
 
+
+from sqlalchemy.exc import IntegrityError
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, abort, Response, current_app
+from Eventhub import db
+from ..models import Event,LoginUser, User
+# from .eventcollection import EventCollection
+# from ..utils import InventoryBuilder, MasonBuilder, create_event_error_response
+import json
+from jsonschema import validate, ValidationError
+
+
+LINK_RELATIONS_URL = "/eventhub/link-relations/"
+USER_PROFILE = "/profiles/user/"
+ERROR_PROFILE = "/profiles/error/"
+MASON = "application/vnd.mason+json"
+
+EVENT_PROFILE = "/profiles/EVENT/"
+ERROR_PROFILE = "/profiles/error/"
 
 class EventItem(Resource):
-
+    api = Api(current_app)
     def get(self, handle):
-        db_product = Product.query.filter_by(handle=handle).first()
-        if db_product is None:
+    
+        db_event = Event.query.filter_by(handle=handle).first()
+        if db_event is None:
             return create_error_response(404, "Not found",
-                                         "No product was found with the handle {}".format(
+                                         "No event was found with the handle {}".format(
                                              handle)
                                          )
 
         body = InventoryBuilder(
-            id=EventItem.id,
-            weight=db_product.weight,
-            price=db_product.price
+            id=db_event.id,
+            name=db_event.name,
+            description=db_event.description,
+            place=db_event.place,
+            time=db_event.time
 
         )
-        body.add_namespace("prohub", LINK_RELATIONS_URL)
-        body.add_control("self", api.url_for(ProductItem, handle=handle))
-        body.add_control("profile", PRODUCT_PROFILE)
-        body.add_control_delete_event(handle)
-        body.add_control_edit_event(handle)
+        body.add_namespace("eventhub", LINK_RELATIONS_URL)
+        body.add_control("self", api.url_for(EventItem, id=id))
+        body.add_control("profile", Event_PROFILE)
+        body.add_control_delete_event(id)
+        body.add_control_edit_event(id)
         body.add_control_all_event()
-
+        print("success!")
         return Response(json.dumps(body), 200, mimetype=MASON)
-
+    
+        return 
     def put(self, handle):
         return
